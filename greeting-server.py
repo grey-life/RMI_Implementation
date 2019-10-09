3ae26fc0e031e5911b88ecb07f28aa0ca938f1f9
greylife <aravind.u.varier@gmail.com>
2019-10-09 21:58:29 +0530

    Added Server and Client Files

---
+++ greeting-server.py
@@ -0,0 +1,17 @@
+import Pyro4
+
+@Pyro4.expose
+
+class GreetingMaker(Object):
+    def get_fortunate(self, name):
+        greeting = 'Hello, '+name + "."
+        fortune_message = '\nSomeone is looking upto you. Don\'t let that person down'
+        return greeting + fortune_message
+
+daemon = Pyro4.Daemon()
+uri = daemon.register(GreetingMaker)
+
+print("Ready. Object uri = ", uri)
+daemon.requestLoop()
+
+
