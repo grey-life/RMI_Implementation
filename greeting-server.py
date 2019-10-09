de4a3db1f3f573f12f98315ab0c6d800ea730ded
greylife <aravind.u.varier@gmail.com>
2019-10-09 22:11:10 +0530

    Passing Object uri

--- greeting-server.py
+++ greeting-server.py
@@ -5,11 +5,13 @@ import Pyro4
 class GreetingMaker(object):
     def get_fortunate(self, name):
         greeting = 'Hello, '+name + "."
-        fortune_message = '\nSomeone is looking upto you. Don\'t let that person down'
+        fortune_message = '\nSomeone is looking upto you. Don\'t let that person down.'
         return greeting + fortune_message
 
 daemon = Pyro4.Daemon()
+ns = Pyro4.locateNS()
 uri = daemon.register(GreetingMaker)
+ns.register("example.greeting",uri)
 
 print("Ready. Object uri = ", uri)
 daemon.requestLoop()
